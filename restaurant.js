409d2c864c58cae1df31099bee6f6e786ae2643d
theluispestana <33699276+theluispestana@users.noreply.github.com>
2017-12-13 17:55:05 -0500

    Added animation for dropdown menu. Still need to fix the icons.

---
+++ restaurant.js
@@ -0,0 +1,23 @@
+let navMenu = document.querySelector('.nav-menu');
+let iconButton = document.querySelectorAll('.icons');
+
+function toggleMenu() {
+  if (iconButton[0].classList.contains('hide')) {
+    iconButton[0].classList.remove('hide');
+    iconButton[0].classList.add('active');
+    iconButton[1].classList.add('hide');
+    navMenu.classList.remove('hide');
+    navMenu.classList.add('active');
+  }
+  else {
+    iconButton[1].classList.remove('hide');
+    iconButton[0].classList.add('hide');
+    iconButton[0].classList.remove('active');
+    navMenu.classList.add('hide');
+    navMenu.classList.remove('active');
+  }
+}
+
+for (var i = 0; i < iconButton.length; i++) {
+  iconButton[i].addEventListener('click', toggleMenu);
+}
