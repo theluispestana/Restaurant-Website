946cefdeea5f35f62ffea69e86e9ee481571940d
theluispestana <33699276+theluispestana@users.noreply.github.com>
2017-12-21 00:43:23 -0500

    Added animation for grey scale transition.

--- restaurant.js
+++ restaurant.js
@@ -1,4 +1,6 @@
 let pageScroll;
+let attribute;
+let image;
 let navMenu = document.querySelector('.nav-menu');
 let iconButton = document.querySelectorAll('.icons');
 let firstSlide = document.querySelector('.first-slide');
@@ -7,6 +9,7 @@ let quoteText = document.querySelector('.quote-text');
 let quoteBoxHeight = document.querySelector('.quote-box').offsetHeight;
 let aboutMe = document.querySelector('.about-box');
 let aboutHeight = document.querySelector('.about-container').offsetHeight;
+let images = document.querySelectorAll('.images img');
 
 function toggleMenu() {
   if (iconButton[0].classList.contains('hide')) {
@@ -38,7 +41,20 @@ function slide() {
   }
 }
 
-window.addEventListener('scroll', slide);
+function  saturation () {
+  if (event.type == 'mouseenter') {
+    attribute = this.getAttribute('data-image');
+    image = document.querySelector(`img[data-image="${attribute}"]`);
+    image.style.filter = "grayscale(0%)";
+  }
+  else if (event.type == 'mouseleave') {
+    attribute = this.getAttribute('data-image');
+    image = document.querySelector(`img[data-image="${attribute}"]`);
+    image.style.filter = "grayscale(90%)";
+  }
+}
+
+// function  desaturate()
 
 //adding event listener for load animation
 window.onload = slideShrink;
@@ -46,4 +62,11 @@ window.onload = slideShrink;
 //Adding event listeners to both buttons
 for (var i = 0; i < iconButton.length; i++) {
   iconButton[i].addEventListener('click', toggleMenu);
+}
+
+window.addEventListener('scroll', slide);
+
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener('mouseenter', saturation);
+  images[i].addEventListener('mouseleave', saturation);
 }
